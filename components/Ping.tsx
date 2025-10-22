2536d1eef5dc488da6b6eb3552ef7d3f6da19e39
Nicola <61830443+nicola02nb@users.noreply.github.com>
2025-10-22 13:39:22 +0200

    Optimized Event Subscription

--- components/Ping.tsx
+++ components/Ping.tsx
@@ -12,11 +12,7 @@ import { FluxDispatcher, Text, useEffect, useState } from "@webpack/common";
 import { RTCConnectionStore } from "../stores";
 
 export function PingElement({ variant, parenthesis = true, color }: { variant: TextVariant; parenthesis?: boolean; color?: string; }) {
-    const [ping, setPing] = useState(RTCConnectionStore.getLastPing());
-
-    const updatePing = (_: FluxEvent) => {
-        setPing(RTCConnectionStore.getLastPing());
-    };
+    const [ping, setPing] = useState(() => RTCConnectionStore.getLastPing());
 
     const formatPing = (ping: number | undefined) => {
         if (ping === undefined) return "N/A";
@@ -24,12 +20,14 @@ export function PingElement({ variant, parenthesis = true, color }: { variant: T
     };
 
     useEffect(() => {
+        const updatePing = (_: FluxEvent) => {
+            setPing(RTCConnectionStore.getLastPing());
+        };
+        FluxDispatcher.subscribe("RTC_CONNECTION_PING", updatePing);
         return () => {
             FluxDispatcher.unsubscribe("RTC_CONNECTION_PING", updatePing);
         };
     }, []);
-
-    FluxDispatcher.subscribe("RTC_CONNECTION_PING", updatePing);
 
     return (<Text variant={variant} className="pingDisplay" style={{ color: color ?? "inherit" }}>{formatPing(ping)}</Text>);
 }
